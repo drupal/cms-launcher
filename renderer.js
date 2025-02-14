e2b52bf1368f5f25eec559f288334699314a4424
rikki-iki <rikki.bochow@previousnext.com.au>
2025-02-14 17:45:26 +1100

    Installer theming

--- renderer.js
+++ renderer.js
@@ -1,19 +1,26 @@
 const status = document.getElementById( 'status' );
+const title = document.getElementById( 'title' );
+const loader = document.getElementById( 'loader' );
 
 drupal.onInstallStart(() => {
+    title.innerHTML = 'Installing...'
     status.innerText = '☕️ Installing dependencies. This may take several minutes, but only needs to be done once.';
+    loader.innerHTML = '<div class="cms-installer__loader"></div>'
 });
 
 drupal.onInstalled(() => {
+    title.innerHTML = 'Installed'
     status.innerHTML = 'Starting web server...';
 });
 
 drupal.onReady(( url ) => {
+    title.innerHTML = 'Installation complete'
     status.innerHTML = `Up and running at <code>${url}</code>`;
 
     const wrapper = document.getElementById( 'open' );
-    wrapper.innerHTML = `<button>Visit Site</button>`;
+    wrapper.innerHTML = `<button class="button" type="button">Visit Site</button>`;
     wrapper.querySelector( 'button' ).addEventListener( 'click', drupal.open );
+    loader.innerHTML = ''
 });
 
 window.addEventListener( 'load', async () => {
