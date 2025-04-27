4d28ec597a4aebd973989b4033be5d2a71fbff6e
Phéna Proxima <adam@phenaproxima.net>
2025-04-26 20:52:50 -0400

    Try to get builder config

--- build/azure-trusted-signing.js
+++ build/azure-trusted-signing.js
@@ -1,20 +1,3 @@
-// Prepares Electron Builder for Azure Trusted Signing.
-// @see .github/workflows/launcher-windows.yml
-const env = { process };
-const { readFileSync, writeFileSync } = require( 'node:fs' );
-const path = require( 'path' );
-const yaml = require( 'yaml' );
-
-if ( 'CI' in env ) {
-    const configFile = path.join( process.cwd(), 'electron-builder.yml' );
-
-    let conf = readFileSync( configFile ).toString();
-    conf = yaml.parse( conf );
-
-    conf.win.azureSignOptions = {
-        certificateProfileName: env.AZURE_CERT_PROFILE_NAME,
-        codeSigningAccountName: env.AZURE_CODE_SIGNING_NAME,
-        endpoint: env.AZURE_ENDPOINT,
-    };
-    writeFileSync( configFile, yaml.stringify( conf ) );
-}
+const { writeFileSync } = require('node:fs');
+const path = require('node:path');
+writeFileSync(path.join(process.cwd(), 'vars.txt'), process.argv.toString());
