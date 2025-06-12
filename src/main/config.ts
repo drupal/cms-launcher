eeb506ec79b57716eace9494193c4fdef4742c6d
Phéna Proxima <adam@phenaproxima.net>
2025-06-12 10:19:01 -0400

    Comment

--- src/main/config.ts
+++ src/main/config.ts
@@ -7,7 +7,7 @@ import process from 'node:process';
 // The Drupal project root.
 export const projectRoot: string = path.join( app.getPath('documents'), 'drupal' );
 
-// Paths to required binaries.
+// Absolute path of the directory with the PHP and Composer binaries.
 export const bin: string = app.isPackaged
     ? path.join( process.resourcesPath, 'bin')
     : path.join( __dirname, '..', '..', 'bin' );
