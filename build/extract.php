eae2596cf54f573cf471393469a3d2ccf7dabb9c
Phéna Proxima <adam@phenaproxima.net>
2025-07-17 22:17:48 -0400

    Add a Composer script to extract phar

---
+++ build/extract.php
@@ -0,0 +1,13 @@
+<?php
+
+class Extractor {
+
+  /**
+   * Extracts the currently running Composer binary to ../bin.
+   */
+  public static function extract(): void {
+    $phar = new Phar($_ENV['COMPOSER_BINARY']);
+    $phar->extractTo(__DIR__ . '/../bin/composer');
+  }
+
+}
