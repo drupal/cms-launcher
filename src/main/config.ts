e55492e4c9386df05e98f0699b0630ec0eb2c6cd
Phéna Proxima <adam@phenaproxima.net>
2025-07-22 09:53:53 -0400

    Allow the project root to be overridden at the command line, and a project template fixture directory to be used in development, for testing purposes.

--- src/main/config.ts
+++ src/main/config.ts
@@ -3,9 +3,28 @@
 import { app } from 'electron';
 import path from 'node:path';
 import process from 'node:process';
+import yargs from 'yargs';
+import { hideBin } from 'yargs/helpers';
+
+// Parse the command line so we can override certain things for testing.
+const argv = yargs(
+        hideBin(process.argv),
+    )
+    .option('root', {
+        type: 'string',
+        description: 'The absolute path to the Drupal project root.',
+        default: path.join(app.getPath('documents'), 'drupal'),
+    })
+    .option('fixture', {
+        type: 'string',
+        description: 'The path of a fixture from which to create the Drupal project. This is internal and only used for testing.',
+        default: null,
+        hidden: true,
+    })
+    .parse();
 
 // The Drupal project root.
-export const projectRoot: string = path.join(app.getPath('documents'), 'drupal');
+export const projectRoot: string = argv.root;
 
 // Where additional resources are stored. This varies depending on whether the app has
 // been packaged for release.
@@ -39,6 +58,14 @@ export const installCommands: string[][] = [
     // if dependencies were being installed at that time.
     ['drupal:recipe-unpack'],
 ];
+
+// Only allow a fixture to be used if the app is not packaged (i.e., during development or
+// when running tests).
+if (argv.fixture && ! app.isPackaged) {
+    // The option does not need to be escaped or quoted, because Composer is not being
+    // executed through a shell.
+    installCommands[0].push(`--repository={"type":"path","url":"${argv.fixture}"}`);
+}
 
 // The absolute path of the web root.
 // @todo Determine this dynamically at install time, and store it.
---
+++ tests/mock-project/web/.ht.router.php
@@ -0,0 +1,6 @@
+<?php
+
+// A mocked version of .ht.router.php, which is normally placed here by
+// Drupal's scaffolding plugin.
+
+require $_SERVER['SCRIPT_FILENAME'];
