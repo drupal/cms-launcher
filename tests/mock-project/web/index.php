6fdab624ca6415f87ab989754148723c40a4b676
Phéna Proxima <adam@phenaproxima.net>
2025-07-22 09:07:01 -0400

    Create a mock version of the `drupal/cms` project template for testing.

---
+++ tests/mock-project/web/index.php
@@ -0,0 +1,5 @@
+<!doctype html>
+<body>
+  <h1>It worked!</h1>
+  <p>Running PHP via <?php echo PHP_SAPI; ?>.</p>
+</body>
---
+++ tests/mock-project/web/sites/default/default.settings.php
@@ -0,0 +1,4 @@
+<?php
+
+// A mocked version of default.settings.php, which is normally placed here by
+// Drupal's scaffolding plugin.
