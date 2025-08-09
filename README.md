eea7714cae92c551f2a65302eb7906fd285c7ef1
Phéna Proxima <adam@phenaproxima.net>
2025-08-08 20:26:43 -0400

    Gather binaries as part of installing Yarn dependencies.

--- README.md
+++ README.md
@@ -74,12 +74,6 @@ Found a bug or want to suggest a feature? [Open an issue](https://github.com/dru
 Clone this repository, then `cd` into it and run:
 
 ```shell
-mkdir bin
-ln -s -f $(which php) bin
-cd bin
-cp $(which composer) composer.phar
-phar extract -f composer.phar ./composer
-cd ..
 yarn install
 yarn run start
 ```
--- build/scripts.php
+++ build/scripts.php
@@ -1,13 +1,19 @@
 <?php
 
+use Composer\Util\Filesystem;
+
 class Scripts {
 
   /**
-   * Extracts the currently running Composer binary to ../bin.
+   * Extracts the Composer binary, and symlinks PHP, into ../bin.
    */
   public static function extract(): void {
-    $phar = new Phar($_ENV['COMPOSER_BINARY']);
-    $phar->extractTo(__DIR__ . '/../bin/composer');
+    $bin = __DIR__ . '/../bin';
+
+    (new Phar($_ENV['COMPOSER_BINARY']))->extractTo($bin . '/composer');
+
+    (new Filesystem)
+      ->relativeSymlink(PHP_BINARY, $bin . '/' . basename(PHP_BINARY));
   }
 
 }
