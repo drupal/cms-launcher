e6190f1a45484e3fdf86a5491c14256a24d9248e
mccoyweb <erik.mccoy@gmail.com>
2025-08-12 11:52:06 -0400

    Complete happy path test

--- playwright.config.ts
+++ playwright.config.ts
@@ -17,9 +17,6 @@ export default defineConfig({
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://localhost:3000',
-
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
   },
@@ -31,11 +28,4 @@ export default defineConfig({
       use: { ...devices['Desktop Firefox'] },
     },
   ],
-
-  /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://localhost:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
 });
--- tests/main.spec.ts
+++ tests/main.spec.ts
@@ -1,14 +1,16 @@
 import { test, expect, _electron as electron } from '@playwright/test';
 import { rm } from 'node:fs/promises';
 import { join } from 'node:path';
+import { installLog } from '../src/main/config';
 
 
-test.afterAll(async ({}, testInfo) => {
-  rm(join(testInfo.outputDir, 'drupal'), { recursive:true } );
+test.afterEach(async ({}, testInfo) => {
+  await rm(join(testInfo.outputDir, 'drupal'), { recursive:true } );
+  /*await testInfo.attach('installLog', { path:installLog } );*/
 });
 
-test('happy path test', async ({}, testInfo) => {
-  test.setTimeout(10_000);
+test('happy path', async ({}, testInfo) => {
+
   const launchOptions = {
     args: [
       '.',
@@ -26,19 +28,17 @@ test('happy path test', async ({}, testInfo) => {
   
   // Launch Electron app.
   const electronApp = await electron.launch(launchOptions);
-  const isPackaged = await electronApp.evaluate(async ({ app }) => {
-    // This runs in Electron's main process, parameter here is always
-    // the result of the require('electron') in the main app script.
-    return app.isPackaged;
-  });
-
-  expect(isPackaged).toBe(false);
 
   // Wait for the first BrowserWindow to open
   // and return its Page object.
   const window = await electronApp.firstWindow();
-  await window.waitForTimeout(10_000);
-  await window.screenshot({ path: 'intro.png' });
+  await expect(window.getByText('Your site is running at')).toBeVisible({ timeout: 30_000 });
+  const siteURL = await window.getByText('http').textContent();
+  console.log(siteURL);
+  if (typeof siteURL === 'string') {
+    await window.goto(siteURL);
+  }
+  await expect(window.getByText('It worked!')).toBeVisible();
   // Close app.
-  /*await electronApp.close();*/
+  await electronApp.close();
 })
