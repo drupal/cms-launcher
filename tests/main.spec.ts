2c98a783d6a3dc8af51d202a9afb166e9f7f7cf2
mccoyweb <erik.mccoy@gmail.com>
2025-08-15 11:20:02 -0400

    Add first version of 'clean up' test

--- tests/main.spec.ts
+++ tests/main.spec.ts
@@ -1,6 +1,5 @@
 import { test, expect, _electron as electron } from '@playwright/test';
-import { rm } from 'node:fs/promises';
-import { join } from 'node:path';
+import { access, rm } from 'node:fs/promises';
 
 test.afterEach(async ({}, testInfo) => {
   // Always attach the log, regardless of success or failure.
@@ -8,10 +7,12 @@ test.afterEach(async ({}, testInfo) => {
     path: testInfo.outputPath('app.log'),
   });
 
-  await rm(testInfo.outputPath('drupal'), {
-    force: true,
-    recursive: true,
-  });
+  if (testInfo.title != 'clean up directory on failed install') {
+    await rm(testInfo.outputPath('drupal'), {
+      force: true,
+      recursive: true,
+    });
+  }
 });
 
 test('happy path', async ({}, testInfo) => {
@@ -40,5 +41,33 @@ test('happy path', async ({}, testInfo) => {
   expect(typeof url).toBe('string');
   await window.goto(url);
   await expect(window.getByText('It worked!')).toBeVisible();
+  await electronApp.close();
+});
+
+test('clean up directory on failed install', async ({}, testInfo) => {
+  const electronApp = await electron.launch({
+    args: [
+      '.',
+      `--root=${testInfo.outputPath('drupal')}`,
+      '--fixture=basic',
+      `--log=${testInfo.outputPath('app.log')}`,
+      '--composer=composer-install-error.php',
+    ],
+    env: {
+      // The fixture is located in a path repository, so we want to ensure Composer
+      // makes a full copy of it.
+      COMPOSER_MIRROR_PATH_REPOS: '1',
+      // Disable the network so we don't inadvertently test the internet.
+      COMPOSER_DISABLE_NETWORK: '1',
+    },
+  });
+
+  // Wait for the first BrowserWindow to open and return its Page object, then
+  // wait up to 10 seconds for the success message to appear.
+  const window = await electronApp.firstWindow();
+  await window.waitForTimeout(3000);
+  await expect(window.getByText('An imaginary error occurred!')).toBeVisible();
+  // Expect access to throw a "no such file or directory" error
+  await expect(access((testInfo.outputPath('drupal')))).rejects.toThrow();
   await electronApp.close();
 });
