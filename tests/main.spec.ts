0ace142b153f14627e0b817f1810d2ee88903c5e
mccoyweb <erik.mccoy@gmail.com>
2025-08-15 16:23:45 -0400

    Refactoring main test file

--- tests/main.spec.ts
+++ tests/main.spec.ts
@@ -1,28 +1,19 @@
 import { test, expect, _electron as electron } from '@playwright/test';
-import { access, rm } from 'node:fs/promises';
+import { accessSync } from 'node:fs';
+import { rm } from 'node:fs/promises';
 
-test.afterEach(async ({}, testInfo) => {
-  // Always attach the log, regardless of success or failure.
-  await testInfo.attach('log', {
-    path: testInfo.outputPath('app.log'),
-  });
-
-  if (testInfo.title != 'clean up directory on failed install') {
-    await rm(testInfo.outputPath('drupal'), {
-      force: true,
-      recursive: true,
-    });
-  }
-});
-
-test('happy path', async ({}, testInfo) => {
-  const electronApp = await electron.launch({
-    args: [
+async function launchApp(root: string, fixture: string, log: string, composer?: string) {
+  const args = [
       '.',
-      `--root=${testInfo.outputPath('drupal')}`,
-      '--fixture=basic',
-      `--log=${testInfo.outputPath('app.log')}`,
-    ],
+      `--root=${root}`,
+      `--fixture=${fixture}`,
+      `--log=${log}`,
+    ];
+  if (composer) {
+    args.push(`--composer=${composer}`);
+  }
+  return electron.launch({
+    args: args,
     env: {
       // The fixture is located in a path repository, so we want to ensure Composer
       // makes a full copy of it.
@@ -31,6 +22,30 @@ test('happy path', async ({}, testInfo) => {
       COMPOSER_DISABLE_NETWORK: '1',
     },
   });
+}
+
+test.afterEach(async ({}, testInfo) => {
+  // Always attach the log, regardless of success or failure.
+  await testInfo.attach('log', {
+    path: testInfo.outputPath('app.log'),
+  });
+
+  if (!testInfo.tags.includes('@cleans-up')) {
+    await rm(testInfo.outputPath('drupal'), {
+      force: true,
+      recursive: true,
+    });
+  }
+});
+
+test('happy path', async ({}, testInfo) => {
+  const root = testInfo.outputPath('drupal');
+
+  const electronApp = await launchApp(
+    root,
+    'basic',
+    testInfo.outputPath('app.log'),
+  );
 
   // Wait for the first BrowserWindow to open and return its Page object, then
   // wait up to 10 seconds for the success message to appear.
@@ -44,30 +59,25 @@ test('happy path', async ({}, testInfo) => {
   await electronApp.close();
 });
 
-test('clean up directory on failed install', async ({}, testInfo) => {
-  const electronApp = await electron.launch({
-    args: [
-      '.',
-      `--root=${testInfo.outputPath('drupal')}`,
-      '--fixture=basic',
-      `--log=${testInfo.outputPath('app.log')}`,
-      '--composer=composer-install-error.php',
-    ],
-    env: {
-      // The fixture is located in a path repository, so we want to ensure Composer
-      // makes a full copy of it.
-      COMPOSER_MIRROR_PATH_REPOS: '1',
-      // Disable the network so we don't inadvertently test the internet.
-      COMPOSER_DISABLE_NETWORK: '1',
-    },
-  });
+test('clean up directory on failed install', {
+  tag: '@cleans-up',
+}, async ({}, testInfo) => {
+  const root = testInfo.outputPath('drupal');
+
+  const electronApp = await launchApp(
+    root,
+    'basic',
+    testInfo.outputPath('app.log'),
+    'composer-install-error.php',
+  );
 
   // Wait for the first BrowserWindow to open and return its Page object, then
   // wait up to 10 seconds for the success message to appear.
   const window = await electronApp.firstWindow();
   await window.waitForTimeout(3000);
   await expect(window.getByText('An imaginary error occurred!')).toBeVisible();
+  await expect(window.locator('.error')).toBeVisible();
   // Expect access to throw a "no such file or directory" error
-  await expect(access((testInfo.outputPath('drupal')))).rejects.toThrow();
+  expect(() => accessSync(root)).toThrow();
   await electronApp.close();
 });
