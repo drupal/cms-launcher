073d67ec10afc8de7050edde59ef9207041fb14a
Phéna Proxima <adam@phenaproxima.net>
2025-10-01 22:37:12 -0400

    Run xvfb in the background directly

--- tests/main.spec.ts
+++ tests/main.spec.ts
@@ -21,6 +21,8 @@ async function launchApp(testInfo: TestInfo, ...options: string[]): Promise<[Ele
       COMPOSER_MIRROR_PATH_REPOS: '1',
       // Disable the network so we don't inadvertently test the internet.
       COMPOSER_DISABLE_NETWORK: '1',
+      // For xvfb on Linux.
+      DISPLAY: process.env.DISPLAY || '',
     },
   });
   return [app, root];
