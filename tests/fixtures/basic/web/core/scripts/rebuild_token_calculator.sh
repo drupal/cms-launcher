09287689438c3888aed41752f9ba5fc50c0894c0
Adam G-H <adam@phenaproxima.net>
2025-12-30 12:21:37 -0500

    Minor cleanup

--- tests/fixtures/basic/web/core/scripts/rebuild_token_calculator.sh
+++ tests/fixtures/basic/web/core/scripts/rebuild_token_calculator.sh
@@ -1,9 +1,7 @@
 <?php
 
-/**
- * @file
- * Simulates an error during a cache clear.
- */
+declare(strict_types=1);
 
+// Simulates an error occurring during a cache clear.
 sleep(1);
-throw new Exception('A fake error during cache clear.');
+throw new \Exception('A fake error while clearing the cache.');
