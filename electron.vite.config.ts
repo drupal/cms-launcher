12038875fdf36f7e5de2bfd4eff4d0153f4382a7
Adam G-H <adam@phenaproxima.net>
2026-02-11 18:05:43 -0500

    Generate source map for the main process and upload it to Sentry

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -1,9 +1,19 @@
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 import { defineConfig } from 'electron-vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 import svg from '@poppanator/sveltekit-svg'
 
 export default defineConfig({
     main: {
+        build: {
+            sourcemap: true,
+        },
+        plugins: [
+            sentryVitePlugin({
+                org: "drupal-association",
+                project: "cms-launcher"
+            }),
+        ],
     },
     preload: {
     },
